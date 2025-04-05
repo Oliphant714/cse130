8b1eca45b29948f852d6fab5d825245066057868
Oliphant <131810276+Oliphant714@users.noreply.github.com>
2025-04-05 14:21:09 -0600

    json file updates

--- W11/practice.py
+++ W11/practice.py
@@ -1,7 +1,28 @@
-number_list = [41, 45, 47, 32, 49, 40, 32]
-window_size = 4
+import json
+
+filename = input("What is the name of the file? ").lower()
+correctfile = False
+
+while not correctfile:
+    if not filename.endswith(".json"):
+        filename += ".json"
+    try:
+        with open(f'W11/{filename}', 'rt') as filehandle:
+            data = json.load(filehandle)
+
+            assert "array" in data
+            assert isinstance(data["array"], list)
+            
+            correctfile = True
+    except (FileExistsError, FileNotFoundError):
+        print("File not found.")
+        filename = input("What is the name of the file? ").lower()
+
+window_size = int(input("Enter the window size: "))
 
 def highest_average(array, window_size):
+    assert window_size > 0, "Window size must be greater than 0"
+    assert window_size <= len(array), "Window size must be less than or equal to the length of the array"
     window_sum = sum(array[:window_size])
     max_sum = window_sum
 
@@ -10,4 +31,4 @@ def highest_average(array, window_size):
         max_sum = max(max_sum, window_sum)
     return max_sum / window_size
 
-print("The highest average of the list is:", highest_average(number_list, window_size))
+print("The highest average of the list is:", highest_average(data["array"], window_size))
